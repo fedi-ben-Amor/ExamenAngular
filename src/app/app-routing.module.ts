b7648610958b59ad5c43800cecdab0164faa8adb
unknown <Haelkyll@gmail.com>
2024-01-08 22:58:11 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  /*{path:'404',component:NotfoundComponent},
+  {path:'**',redirectTo:'404',pathMatch:'full'}*/
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
